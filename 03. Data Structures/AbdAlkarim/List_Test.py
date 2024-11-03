7e3f7d54243df263db4e72dcf9ec1096a4cb9506
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-11-03 18:28:55 +0200

    Update List_Test.py

--- 03. Data Structures/AbdAlkarim/List_Test.py
+++ 03. Data Structures/AbdAlkarim/List_Test.py
@@ -1,15 +1,16 @@
-# list1 = [2,3,4,1,32]
-# print(len(list1))
+list1 = [2,3,4,1,32]
+print(list1)
+print("The Leng of list = ", len(list1))
 
-# print(max(list1))
+print("The Max Value from list = " ,max(list1))
 
-# print(min(list1))
+print("The Min Value from list = " ,min(list1))
 
-# print(sum(list1))
+print("The Sum Value from list = " ,sum(list1))
 
-# import random
-# random.shuffle(list1)
-# print(list1)
+import random
+random.shuffle(list1)
+print("The shuffle number is: ",list1)
 ########################################################################
 # list1 = [2,3]
 # list2 = [1,9]
@@ -22,11 +23,11 @@
 # print(list4)
 #########################################################################
 
-list1 = [2,3,5,2,33,21]
-print(list1[-1])
-print(list1[-3])
-print(list1)
-print(2 in list1)
-print(2.5 in list1)
+# list1 = [2,3,5,2,33,21]
+# print(list1[-1])
+# print(list1[-3])
+# print(list1)
+# print(2 in list1)
+# print(2.5 in list1)
 
-print(3 in list1)
+# print(3 in list1)
