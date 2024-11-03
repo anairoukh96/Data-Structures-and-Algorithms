9d6c52df74aac573da7e08544e357d55fccf36a2
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-11-03 18:22:05 +0200

    Create List_Test.py

---
+++ 03. Data Structures/AbdAlkarim/List_Test.py
@@ -0,0 +1,31 @@
+# list1 = [2,3,4,1,32]
+# print(len(list1))
+
+# print(max(list1))
+
+# print(min(list1))
+
+# print(sum(list1))
+
+# import random
+# random.shuffle(list1)
+# print(list1)
+########################################################################
+# list1 = [2,3]
+# list2 = [1,9]
+# list3 = list1 + list2
+# print(list3)
+# list3 = 3 * list1
+# print(list3)
+
+# list4 = list3[2:4]
+# print(list4)
+#########################################################################
+
+list1 = [2,3,5,2,33,21]
+print(list1[-1])
+print(list1[-3])
+print(list1)
+print(2 in list1)
+print(2.5 in list1)
+
