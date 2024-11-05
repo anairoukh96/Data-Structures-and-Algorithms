85f8bc51eef37bcc1305558ed688839adccdf3c7
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-11-05 08:23:06 +0200

    Update List_Test_1.py

--- 03. Data Structures/AbdAlkarim/List_Test_1.py
+++ 03. Data Structures/AbdAlkarim/List_Test_1.py
@@ -1,7 +1,17 @@
-list1=[2,3,5,2,33,21]
+# list1=[2,3,5,2,33,21]
 
-i = 0
-while i < len(list1):
-    print(list1[i], end=" ")
-    i += 1
+# i = 0
+# while i < len(list1):
+#     print(list1[i], end=" ")
+#     i += 1
 
+######################################################
+
+list1 = [x for x in range(0,11)]
+print(list1)
+
+list2 = [0.5 * x for x in list1]
+print(list2)
+
+list3 = [x for x in list2 if x < 1.5]
+print(list3)
