86740901436204f2648329612f7201103156c036
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-11-12 07:58:53 +0200

    Update List_Test_2.py

--- 03. Data Structures/AbdAlkarim/List_Test_2.py
+++ 03. Data Structures/AbdAlkarim/List_Test_2.py
@@ -17,10 +17,14 @@
 
 ################################################################################
 
-def linearSearch(lst, key):
-    for i in range(0 , len(lst)):
-        if key == lst[i]:
-            return i
-    return -1
-list1 = [2,3,5,7]
-print(linearSearch(list1,5))
+# def linearSearch(lst, key):
+#     for i in range(0 , len(lst)):
+#         if key == lst[i]:
+#             return i
+#     return -1
+# list1 = [2,3,5,7]
+# print(linearSearch(list1,5))
+
+################################################################################
+
+print("")
