53d30b884c80dd858cc07c83a9ceef7967dd5fb8
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-12-04 09:57:37 +0200

    Create dictionary.py

---
+++ 03. Data Structures/AbdAlkarim/dictionary.py
@@ -0,0 +1,14 @@
+dictionary = {"Karim":22210145,"Mohammad":22210198,"Naser":22210155}
+print(dictionary)
+
+dictionary["Qusai"] = 22310116
+print(dictionary)
+
+print(dictionary["Qusai"])
+
+del dictionary["Qusai"]
+print(dictionary)
+
+print("Karim" in dictionary)
+dictionary.keys()
+print(dictionary)
