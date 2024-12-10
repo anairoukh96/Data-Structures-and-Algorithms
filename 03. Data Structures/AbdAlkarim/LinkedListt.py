08b9856831704dbc1ae36711efda2a966eb383e6
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-12-10 09:41:24 +0200

    Update LinkedListt.py

--- 03. Data Structures/AbdAlkarim/LinkedListt.py
+++ 03. Data Structures/AbdAlkarim/LinkedListt.py
@@ -1,4 +1,23 @@
 def LinkedList():
-    from LinkedList import LinkedList
-    lst = LinkedList
+    from LinkedList import LinkedList        # from اسم الملف  import اسم فانكشين
+    lst = LinkedList()
     
+    lst.add("Hebron")
+    print("(1)", lst)
+
+    lst.add("Ramallah")
+    print("(2)", lst)
+
+    lst.addLast("Nablus")
+    print("(3)", lst)
+
+    lst.addLast("Jenin")
+    print("(4)", lst)
+    
+    lst.addLast("Tolkarm")
+    print("(5)", lst)
+
+    lst.addLast("Betlahim")
+    print("(6)", lst)
+
+LinkedList()
