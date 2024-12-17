747e43ca4a8d065fc29895d1d8ab0949491d7395
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-12-17 08:10:16 +0200

    Create Quiz.py

---
+++ 03. Data Structures/AbdAlkarim/LinkedList/Quiz.py
@@ -0,0 +1,13 @@
+from LinkedList import LinkedList
+lst = LinkedList()
+lst.add("B")
+lst.add("i")
+lst.add("t")
+lst.add("c")
+lst.add("o")
+lst.add("i")
+lst.add("n")
+
+for e in lst:
+    print(e, end = ' ')
+print()
