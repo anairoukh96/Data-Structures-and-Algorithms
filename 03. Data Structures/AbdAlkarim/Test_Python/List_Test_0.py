d92ee1886e458a8193b6db56efb80c8c761f7dcd
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-12-23 06:17:06 +0200

    Create List_Test_0.py

---
+++ 03. Data Structures/AbdAlkarim/Test_Python/List_Test_0.py
@@ -0,0 +1,35 @@
+# list1 = [2,3,4,1,32]
+# print(list1)
+# print("The Leng of list = ", len(list1))
+
+# print("The Max Value from list = " ,max(list1))
+
+# print("The Min Value from list = " ,min(list1))
+
+# print("The Sum Value from list = " ,sum(list1))
+
+# import random
+# random.shuffle(list1)
+# print("The shuffle number is: ",list1)
+
+########################################################################
+# list1 = [2,3]
+# list2 = [1,9]
+# list3 = list1 + list2
+# print("The full list is: ", list3)
+
+# list3 = 3 * list1
+# print(list3)
+
+# list4 = list3[2:4]
+# print(list4)
+#########################################################################
+
+# list1 = [2,3,5,2,33,21]
+# print(list1,"\n","*" * 30)
+# print("List [-1] is: ",list1[-1])
+# print("List [-3] is: ",list1[-3])
+# print(2 in list1) # هل رقم 2 موجود داخل القائمة او لا اذا موجود صحيح واذا لا خاطئ
+# print(2.5 in list1)
+
+print("")
