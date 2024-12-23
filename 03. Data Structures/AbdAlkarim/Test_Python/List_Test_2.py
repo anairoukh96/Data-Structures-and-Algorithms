8dca271d5abab07ee5ff93749577c8324e778f1e
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-12-23 06:17:01 +0200

    Create List_Test_2.py

---
+++ 03. Data Structures/AbdAlkarim/Test_Python/List_Test_2.py
@@ -0,0 +1,30 @@
+# def reverse(list):
+#     result = []
+
+#     for element in list:
+#         result.insert(0, element)
+#     return result
+
+# list1 = [1,2,3,4,5,6]
+# list2 = reverse(list1)
+# print(list2)
+
+################################################################################
+
+# list3 = [2,3,5,7]
+# list3.reverse()
+# print('Reversed List:', list3)
+
+################################################################################
+
+# def linearSearch(lst, key):
+#     for i in range(0 , len(lst)):
+#         if key == lst[i]:
+#             return i
+#     return -1
+# list1 = [2,3,5,7]
+# print(linearSearch(list1,5))
+
+################################################################################
+
+print("")
