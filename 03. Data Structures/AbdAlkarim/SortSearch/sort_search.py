1f7099c1e40ae29e27d5d908beafdb344d999109
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2025-01-15 06:18:47 +0200

    Update sort_search.py

---
+++ 03. Data Structures/AbdAlkarim/SortSearch/sort_search.py
@@ -0,0 +1,9 @@
+def selectionSort(lst):
+    for i in range(len(lst)-1):
+        currentMin = min(lst[i : ])
+        currentMinIndex = i + lst[i : ].index(currentMin)
+        if currentMinIndex != i:
+            lst[currentMinIndex], lst[i] = lst[i], currentMin
+    return lst
+list1 = [4,5,99,8,65,46,82,130,52]
+print(selectionSort(list1))
